d96b1661cb491b05d196cc33041f04f3d14819a7
Deepika Vusarthi <123539808+Deepikavusarthi@users.noreply.github.com>
2026-04-08 19:57:55 -0500

    Completed Part 7 implement create

--- controllers/costume.js
+++ controllers/costume.js
@@ -27,14 +27,28 @@ exports.costume_detail = function(req, res) {
   res.send('NOT IMPLEMENTED: Costume detail: ' + req.params.id);
 };
 
-exports.costume_create_post = function(req, res) {
-  res.send('NOT IMPLEMENTED: Costume create POST');
+// Handle costume create on POST
+exports.costume_create_post = async function(req, res) {
+  console.log(req.body);
+  let document = new Costume();
+  document.costume_type = req.body.costume_type;
+  document.cost = req.body.cost;
+  document.size = req.body.size;
+  try {
+    let result = await document.save();
+    res.send(result);
+  } catch (err) {
+    res.status(500);
+    res.send({ error: `${err}` });
+  }
 };
 
+// Handle costume delete on DELETE
 exports.costume_delete = function(req, res) {
   res.send('NOT IMPLEMENTED: Costume delete DELETE ' + req.params.id);
 };
 
+// Handle costume update on PUT
 exports.costume_update_put = function(req, res) {
   res.send('NOT IMPLEMENTED: Costume update PUT ' + req.params.id);
 };
