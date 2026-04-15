1b7788ec59a5752211e7c6f211220160203d0aeb
Deepika Vusarthi <123539808+Deepikavusarthi@users.noreply.github.com>
2026-04-15 14:27:57 -0500

    Added detail page view

--- app.js
+++ app.js
@@ -7,15 +7,22 @@ dotenv.config();
 
 const app = express();
 
+// Views
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "pug");
 
+// Middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+// ✅ IMPORTANT (for CSS)
+app.use(express.static('public'));
+
+// Routes
 const resourceRouter = require("./routes/resource");
 const costumesRouter = require("./routes/costumes");
 
+// MongoDB
 mongoose.connect(process.env.MONGO_CON);
 
 const db = mongoose.connection;
@@ -24,13 +31,16 @@ db.once("open", function () {
   console.log("Connected to MongoDB Atlas");
 });
 
+// Test route
 app.get("/", function(req, res) {
   res.send("Mongo App is running");
 });
 
+// Use routes
 app.use("/resource", resourceRouter);
 app.use("/costumes", costumesRouter);
 
+// Server
 app.listen(3000, function () {
   console.log("Server running on port 3000");
 });
