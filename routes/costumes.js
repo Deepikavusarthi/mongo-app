afa38e49ad3ad875e78059c6290a7e96348e7079
Deepika Vusarthi <123539808+Deepikavusarthi@users.noreply.github.com>
2026-04-22 14:54:15 -0500

    Protected update route with authentication middleware

--- routes/costumes.js
+++ routes/costumes.js
@@ -2,11 +2,27 @@ var express = require('express');
 var router = express.Router();
 var costume_controller = require('../controllers/costume');
 
+// Middleware to check if user is logged in
+const secured = (req, res, next) => {
+  if (req.user) {
+    return next();
+  }
+  res.redirect("/login");
+};
+
 /* GET home page for costumes */
 router.get('/', costume_controller.costume_view_all_Page);
+
+/* GET detail costume page */
 router.get('/detail', costume_controller.costume_view_one_Page);
-router.get('/create', costume_controller.costume_create_Page);
-router.get('/update', costume_controller.costume_update_Page);
-router.get('/delete', costume_controller.costume_delete_Page);
+
+/* GET create costume page */
+router.get('/create', secured, costume_controller.costume_create_Page);
+
+/* GET update costume page */
+router.get('/update', secured, costume_controller.costume_update_Page);
+
+/* GET delete costume page */
+router.get('/delete', secured, costume_controller.costume_delete_Page);
 
 module.exports = router;
